b78eeabf50cb3fd9c2a055a76915a0a5087d0b7b
Louis Fry-Bouriaux <lfrybouriaux@seeqc.com>
2026-04-29 04:47:53 +0100

    Refactor graph traversal logic

--- pyscqed/evaluation_graph.py
+++ pyscqed/evaluation_graph.py
@@ -3,30 +3,62 @@ from typing import Callable, Any
 import networkx as nx
 
 
+def _define_outputs(fn: Callable, keys: list[str]) -> Callable:
+    """Decorator that maps function outputs to user defined keys."""
+    def inner(*args, **kwargs) -> dict[str, Any]:
+        outputs = fn(*args, **kwargs)
+        if not isinstance(outputs, tuple):
+            if len(keys) != 1:
+                raise TypeError("A singular function output must map to a single key.")
+            return {keys[0]: outputs}
+        formatted = {k: v for k, v in zip(keys, outputs)}
+        return formatted
+    return inner
+
+
 class EvaluationGraph:
     def __init__(self):
         self._graph = nx.DiGraph()
+        self._silent_nodes: set[str] = set()
 
-    def add_node(self, name: str, fn: Callable, inputs: set[str]):
-        self._graph.add_node(name, node_fn=fn, node_inputs=inputs)
+    def add_node(self, name: str, fn: Callable, outputs: list[str]):
+        self._graph.add_node(name, node_fn=fn, node_outputs=outputs)
 
     def add_dependency(self, source_node: str, target_node: str, preserve_source_outputs: bool = False):
         self._graph.add_edge(source_node, target_node)
+        if preserve_source_outputs:
+            self._silent_nodes.add(source_node)
 
-    def evaluate(self, inputs: dict[str, Any]) -> dict[str, Any]:
+    def evaluate(self, inputs: dict[str, dict[str, Any]]) -> dict[str, Any]:
         # Get the start nodes
         start_nodes = {node for node in self._graph.nodes if self._graph.in_degree(node) == 0}
-        data = {
-            node: self._graph.nodes[node]["node_fn"](**inputs[node])
-            for node in start_nodes
-        }
-        new_data = {}
-        for node, inputs in data.items():
+
+        # Produce the outputs of the start nodes
+        data = self._get_node_outputs(start_nodes, inputs)
+
+        # Get next set of nodes
+        next_nodes = set()
+        next_inputs = {}
+        for node in data:
             node_iter = self._graph.successors(node)
-            new_data.update({
-                node: self._graph.nodes[node]["node_fn"](inputs)
-                for node in node_iter
-            })
-        data.update(new_data)
-        #print(data)
+            for inode in node_iter:
+                next_nodes.add(inode)
+
+                # The outputs of the previous function become the inputs of the next
+                next_inputs[inode] = data[node]
+
+        # Produce the next outputs
+        data.update(self._get_node_outputs(next_nodes, next_inputs))
+        return data
+
+    def _get_node_outputs(self, nodes: set[int], inputs: dict[str, dict[str, Any]]) -> dict[str, Any]:
+        data = {}
+        for node in nodes:
+            original_callable = self._graph.nodes[node]["node_fn"]
+            user_defined_outputs = self._graph.nodes[node]["node_outputs"]
+            wrapped_callable = _define_outputs(
+                original_callable,
+                user_defined_outputs
+            )
+            data[node] = wrapped_callable(**inputs[node])
         return data
--- tests/test_evaluation_graph.py
+++ tests/test_evaluation_graph.py
@@ -13,8 +13,8 @@ def test_graph_creation():
         return arg == 1
 
     graph = EvaluationGraph()
-    graph.add_node("A", fn=test_fn1, inputs={"input"})
-    graph.add_node("B", fn=test_fn2, inputs={"arg"})
+    graph.add_node("A", fn=test_fn1, outputs=["arg"])
+    graph.add_node("B", fn=test_fn2, outputs=["final"])
     graph.add_dependency("A", "B", preserve_source_outputs=True)
     inputs = {
         "A": {
@@ -22,8 +22,8 @@ def test_graph_creation():
         }
     }
     result = graph.evaluate(inputs=inputs)
-    assert result["A"] == 1
-    assert result["B"] == True
+    assert result["A"]["arg"] == 1
+    assert result["B"]["final"] == True
 
     inputs = {
         "A": {
@@ -31,5 +31,5 @@ def test_graph_creation():
         }
     }
     result = graph.evaluate(inputs=inputs)
-    assert result["A"] == 0
-    assert result["B"] == False
+    assert result["A"]["arg"] == 0
+    assert result["B"]["final"] == False
