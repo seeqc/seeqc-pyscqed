601bb591aaa439e55e7ddf0a8f6875ce3ae75e67
Louis Fry-Bouriaux <lfrybouriaux@seeqc.com>
2026-07-08 11:31:38 +0100

    Refactor circuit graph tests to use pytest

--- tests/test_circuit_graph.py
+++ tests/test_circuit_graph.py
@@ -1,14 +1,14 @@
-"""Test the circuit graph API."""
-import unittest
 import itertools
 import os
+
+import pytest
 
 import numpy as np
 
 from pyscqed.circuit_graph import CircuitGraph
 
 
-def do_all_branch_type_tests_on_branch(testobj, G, edge, expected_type):
+def do_all_branch_type_tests_on_branch(G, edge, expected_type):
     fn = [
         G.isCapacitiveEdge,
         G.isInductiveEdge,
@@ -16,9 +16,9 @@ def do_all_branch_type_tests_on_branch(testobj, G, edge, expected_type):
     ]
     for i in range(3):
         if i == G._element_prefixes.index(expected_type):
-            testobj.assertTrue(fn[i](edge))
+            assert fn[i](edge)
         else:
-            testobj.assertFalse(fn[i](edge))
+            assert not fn[i](edge)
 
 
 def get_simple_test_graph():
@@ -30,7 +30,7 @@ def get_simple_test_graph():
     return graph
 
 
-def all_loops_are_unique(testobj, G):
+def all_loops_are_unique(G):
     """There should be at least one distinct branch per loop."""
     available = {}
     for i, loop in G.sc_loops.items():
@@ -39,7 +39,7 @@ def all_loops_are_unique(testobj, G):
             if i == j:
                 continue
             curr -= set(other_loop)
-        testobj.assertTrue(len(curr) > 0)
+        assert len(curr) > 0
 
 
 def plot_all_graphs(G, prefix):
@@ -47,230 +47,238 @@ def plot_all_graphs(G, prefix):
         G.drawGraphViz(graph=t, filename=("%s_%s" % (prefix, t)))
 
 
-class CircuitGraphTest(unittest.TestCase):
-    """Test the circuit_graph.CircuitGraph class."""
-
-    def setUp(self):
-        """Run before tests"""
-
-    def tearDown(self):
-        """Run after tests"""
-
-    def test_graph_branch_rules(self):
-        # Test that branch types can be correctly identified
-        for i in range(3):
-            graph = CircuitGraph()
-            graph.addBranch(0, 1, graph._element_prefixes[i])
-            do_all_branch_type_tests_on_branch(self, graph, (0, 1, 0), graph._element_prefixes[i])
-
-        # Cannot add invalid components
-        self.assertRaises(ValueError, graph.addBranch, 0, 1, "J")
-
-        # Test that the correct number of superconducting loops are detected
-        for perm in itertools.permutations(["I", "L"], 2):
-            graph = CircuitGraph()
-            for item in perm:
-                graph.addBranch(0, 1, item)
-            self.assertTrue(len(graph.closure_branches) == 1)
-            self.assertTrue(len(graph.sc_loops) == 1)
-            all_loops_are_unique(self, graph)
-
-        for perm in itertools.permutations(["I1", "I2", "L"], 3):
-            graph = CircuitGraph()
-            for item in perm:
-                graph.addBranch(0, 1, item)
-            self.assertTrue(len(graph.closure_branches) == 2)
-            self.assertTrue(len(graph.sc_loops) == 2)
-            all_loops_are_unique(self, graph)
-
-        for perm in itertools.permutations(["I1", "I2", "I3", "L"], 4):
-            graph = CircuitGraph()
-            for item in perm:
-                graph.addBranch(0, 1, item)
-            self.assertTrue(len(graph.closure_branches) == 3)
-            self.assertTrue(len(graph.sc_loops) == 3)
-            all_loops_are_unique(self, graph)
-
-        # Floating single loop
+def test_graph_branch_rules():
+    # Test that branch types can be correctly identified
+    for i in range(3):
         graph = CircuitGraph()
-        graph.addBranch(0, 1, "I1")
-        graph.addBranch(1, 2, "L1")
-        graph.addBranch(1, 2, "L2")
-        self.assertTrue(len(graph.closure_branches) == 1)
-        self.assertTrue(len(graph.sc_loops) == 1)
-        all_loops_are_unique(self, graph)
+        graph.addBranch(0, 1, graph._element_prefixes[i])
+        do_all_branch_type_tests_on_branch(graph, (0, 1, 0), graph._element_prefixes[i])
 
-        # No conducting loops
+    # Cannot add invalid components
+    with pytest.raises(ValueError):
+        graph.addBranch(0, 1, "J")
+
+    # Test that the correct number of superconducting loops are detected
+    for perm in itertools.permutations(["I", "L"], 2):
         graph = CircuitGraph()
-        graph.addBranch(0, 1, "I")
-        graph.addBranch(1, 2, "C")
-        graph.addBranch(1, 2, "L")
-        self.assertTrue(len(graph.closure_branches) == 0)
-        self.assertTrue(len(graph.sc_loops) == 0)
-        all_loops_are_unique(self, graph)
+        for item in perm:
+            graph.addBranch(0, 1, item)
+        assert len(graph.closure_branches) == 1
+        assert len(graph.sc_loops) == 1
+        all_loops_are_unique(graph)
 
+    for perm in itertools.permutations(["I1", "I2", "L"], 3):
         graph = CircuitGraph()
-        graph.addBranch(0, 1, "I")
-        graph.addBranch(1, 2, "C")
-        graph.addBranch(0, 2, "L")
-        self.assertTrue(len(graph.closure_branches) == 0)
-        self.assertTrue(len(graph.sc_loops) == 0)
-        all_loops_are_unique(self, graph)
+        for item in perm:
+            graph.addBranch(0, 1, item)
+        assert len(graph.closure_branches) == 2
+        assert len(graph.sc_loops) == 2
+        all_loops_are_unique(graph)
 
-        # Large loop
+    for perm in itertools.permutations(["I1", "I2", "I3", "L"], 4):
         graph = CircuitGraph()
-        N = 6
-        for i in range(N):
-            graph.addBranch(
-                i, (i + 1) % N,
-                "%s%i" % (graph._element_prefixes[np.random.randint(2) + 1], i)
-            )
-        self.assertTrue(len(graph.closure_branches) == 1)
-        self.assertTrue(len(graph.sc_loops) == 1)
-        all_loops_are_unique(self, graph)
+        for item in perm:
+            graph.addBranch(0, 1, item)
+        assert len(graph.closure_branches) == 3
+        assert len(graph.sc_loops) == 3
+        all_loops_are_unique(graph)
 
-        # Loops in series forming a larger loop
-        graph = CircuitGraph()
-        graph.addBranch(0, 1, "I1")
-        graph.addBranch(0, 1, "C1")
-        graph.addBranch(0, 1, "L1") # 1 loop on (0, 1, k)
-        graph.addBranch(1, 2, "L2")
-        graph.addBranch(1, 2, "I2")
-        graph.addBranch(1, 2, "C2") # 1 loop on (1, 2, k)
-        graph.addBranch(0, 2, "I3")
-        graph.addBranch(0, 2, "I4")
-        graph.addBranch(0, 2, "L3") # 2 loops on (0, 2, k)
-        self.assertTrue(len(graph.closure_branches) == 5)
-        self.assertTrue(len(graph.sc_loops) == 5)
-        all_loops_are_unique(self, graph)
+    # Floating single loop
+    graph = CircuitGraph()
+    graph.addBranch(0, 1, "I1")
+    graph.addBranch(1, 2, "L1")
+    graph.addBranch(1, 2, "L2")
+    assert len(graph.closure_branches) == 1
+    assert len(graph.sc_loops) == 1
+    all_loops_are_unique(graph)
 
-        # Larger loops sharing a branch (3 in this case)
-        graph = CircuitGraph()
-        graph.addBranch(0, 1, "I1")
-        graph.addBranch(1, 2, "L1a")
-        graph.addBranch(2, 0, "L1b")
-        graph.addBranch(1, 3, "L2a")
-        graph.addBranch(3, 0, "L2b")
-        graph.addBranch(1, 4, "L3a")
-        graph.addBranch(4, 0, "L3b")
-        self.assertTrue(len(graph.closure_branches) == 3)
-        self.assertTrue(len(graph.sc_loops) == 3)
-        all_loops_are_unique(self, graph)
+    # No conducting loops
+    graph = CircuitGraph()
+    graph.addBranch(0, 1, "I")
+    graph.addBranch(1, 2, "C")
+    graph.addBranch(1, 2, "L")
+    assert len(graph.closure_branches) == 0
+    assert len(graph.sc_loops) == 0
+    all_loops_are_unique(graph)
 
-        # Larger loops sharing a loop (3 in this case)
-        graph = CircuitGraph()
-        graph.addBranch(0, 1, "I1")
-        graph.addBranch(0, 1, "I2")
-        graph.addBranch(1, 2, "L1a")
-        graph.addBranch(2, 0, "L1b")
-        graph.addBranch(1, 3, "L2a")
-        graph.addBranch(3, 0, "L2b")
-        graph.addBranch(1, 4, "L3a")
-        graph.addBranch(4, 0, "L3b")
-        self.assertTrue(len(graph.closure_branches) == 4)
-        self.assertTrue(len(graph.sc_loops) == 4)
-        all_loops_are_unique(self, graph)
+    graph = CircuitGraph()
+    graph.addBranch(0, 1, "I")
+    graph.addBranch(1, 2, "C")
+    graph.addBranch(0, 2, "L")
+    assert len(graph.closure_branches) == 0
+    assert len(graph.sc_loops) == 0
+    all_loops_are_unique(graph)
 
-        # Larger loops sharing a large loop (2 in this case)
-        graph = CircuitGraph()
-        graph.addBranch(0, 1, "Lma")
-        graph.addBranch(1, 2, "Ima")
-        graph.addBranch(0, 3, "Lmb")
-        graph.addBranch(3, 2, "Imb")
-        graph.addBranch(0, 4, "I1a")
-        graph.addBranch(4, 2, "L1a")
-        graph.addBranch(0, 5, "I2a")
-        graph.addBranch(5, 2, "L2a")
-        self.assertTrue(len(graph.closure_branches) == 3)
-        self.assertTrue(len(graph.sc_loops) == 3)
-        all_loops_are_unique(self, graph)
+    # Large loop
+    graph = CircuitGraph()
+    N = 6
+    for i in range(N):
+        graph.addBranch(
+            i, (i + 1) % N,
+            "%s%i" % (graph._element_prefixes[np.random.randint(2) + 1], i)
+        )
+    assert len(graph.closure_branches) == 1
+    assert len(graph.sc_loops) == 1
+    all_loops_are_unique(graph)
 
-    def test_component_listers(self):
-        graph = CircuitGraph()
-        graph.addBranch(0, 1, "I1")
-        graph.addBranch(0, 1, "C1")
-        graph.addBranch(0, 1, "I2")
-        graph.addBranch(0, 1, "C2")
-        graph.addBranch(1, 2, "L1a")
-        graph.addBranch(2, 0, "L1b")
-        graph.addBranch(1, 3, "L2a")
-        graph.addBranch(3, 0, "L2b")
-        graph.addBranch(1, 4, "L3a")
-        graph.addBranch(4, 0, "L3b")
+    # Loops in series forming a larger loop
+    graph = CircuitGraph()
+    graph.addBranch(0, 1, "I1")
+    graph.addBranch(0, 1, "C1")
+    graph.addBranch(0, 1, "L1") # 1 loop on (0, 1, k)
+    graph.addBranch(1, 2, "L2")
+    graph.addBranch(1, 2, "I2")
+    graph.addBranch(1, 2, "C2") # 1 loop on (1, 2, k)
+    graph.addBranch(0, 2, "I3")
+    graph.addBranch(0, 2, "I4")
+    graph.addBranch(0, 2, "L3") # 2 loops on (0, 2, k)
+    assert len(graph.closure_branches) == 5
+    assert len(graph.sc_loops) == 5
+    all_loops_are_unique(graph)
 
-        inds = {"L1a", "L1b", "L2a", "L2b", "L3a", "L3b"}
-        jjs = {"I1", "I2"}
-        caps = {"C1", "C2"}
+    # Larger loops sharing a branch (3 in this case)
+    graph = CircuitGraph()
+    graph.addBranch(0, 1, "I1")
+    graph.addBranch(1, 2, "L1a")
+    graph.addBranch(2, 0, "L1b")
+    graph.addBranch(1, 3, "L2a")
+    graph.addBranch(3, 0, "L2b")
+    graph.addBranch(1, 4, "L3a")
+    graph.addBranch(4, 0, "L3b")
+    assert len(graph.closure_branches) == 3
+    assert len(graph.sc_loops) == 3
+    all_loops_are_unique(graph)
 
-        self.assertTrue(set(graph.getInductiveEdges()) == inds)
-        self.assertTrue(set(graph.getJosephsonEdges()) == jjs)
-        self.assertTrue(set(graph.getCapacitiveEdges()) == caps)
+    # Larger loops sharing a loop (3 in this case)
+    graph = CircuitGraph()
+    graph.addBranch(0, 1, "I1")
+    graph.addBranch(0, 1, "I2")
+    graph.addBranch(1, 2, "L1a")
+    graph.addBranch(2, 0, "L1b")
+    graph.addBranch(1, 3, "L2a")
+    graph.addBranch(3, 0, "L2b")
+    graph.addBranch(1, 4, "L3a")
+    graph.addBranch(4, 0, "L3b")
+    assert len(graph.closure_branches) == 4
+    assert len(graph.sc_loops) == 4
+    all_loops_are_unique(graph)
 
-    def test_flux_biasing_rules(self):
-        graph = get_simple_test_graph()
+    # Larger loops sharing a large loop (2 in this case)
+    graph = CircuitGraph()
+    graph.addBranch(0, 1, "Lma")
+    graph.addBranch(1, 2, "Ima")
+    graph.addBranch(0, 3, "Lmb")
+    graph.addBranch(3, 2, "Imb")
+    graph.addBranch(0, 4, "I1a")
+    graph.addBranch(4, 2, "L1a")
+    graph.addBranch(0, 5, "I2a")
+    graph.addBranch(5, 2, "L2a")
+    assert len(graph.closure_branches) == 3
+    assert len(graph.sc_loops) == 3
+    all_loops_are_unique(graph)
 
-        # Can add a flux bias to a JJ branch without loading
+
+def test_component_listers():
+    graph = CircuitGraph()
+    graph.addBranch(0, 1, "I1")
+    graph.addBranch(0, 1, "C1")
+    graph.addBranch(0, 1, "I2")
+    graph.addBranch(0, 1, "C2")
+    graph.addBranch(1, 2, "L1a")
+    graph.addBranch(2, 0, "L1b")
+    graph.addBranch(1, 3, "L2a")
+    graph.addBranch(3, 0, "L2b")
+    graph.addBranch(1, 4, "L3a")
+    graph.addBranch(4, 0, "L3b")
+
+    inds = {"L1a", "L1b", "L2a", "L2b", "L3a", "L3b"}
+    jjs = {"I1", "I2"}
+    caps = {"C1", "C2"}
+
+    assert set(graph.getInductiveEdges()) == inds
+    assert set(graph.getJosephsonEdges()) == jjs
+    assert set(graph.getCapacitiveEdges()) == caps
+
+
+def test_flux_biasing_rules():
+    graph = get_simple_test_graph()
+
+    # Can add a flux bias to a JJ branch without loading
+    graph.addFluxBias("I1", "Z1")
+
+    # Cannot add a flux bias again to the same JJ branch
+    with pytest.raises(ValueError):
         graph.addFluxBias("I1", "Z1")
 
-        # Cannot add a flux bias again to the same JJ branch
-        self.assertRaises(ValueError, graph.addFluxBias, "I1", "Z1")
+    # Cannot use the same suffix twice
+    with pytest.raises(ValueError):
+        graph.addFluxBias("I2", "Z1")
 
-        # Cannot use the same suffix twice
-        self.assertRaises(ValueError, graph.addFluxBias, "I2", "Z1")
+    # Can add a flux bias to the second JJ branch without loading
+    graph.addFluxBias("I2", "Z2")
 
-        # Can add a flux bias to the second JJ branch without loading
-        graph.addFluxBias("I2", "Z2")
+    graph = get_simple_test_graph()
 
-        graph = get_simple_test_graph()
+    # Cannot add a flux bias to a JJ branch with loading
+    with pytest.raises(TypeError):
+        graph.addFluxBias("I1", "Z1", "M1")
 
-        # Cannot add a flux bias to a JJ branch with loading
-        self.assertRaises(TypeError, graph.addFluxBias, "I1", "Z1", "M1")
+    # Can add a flux bias to a inductive branch with loading
+    graph.addFluxBias("L", "Z", "M")
 
-        # Can add a flux bias to a inductive branch with loading
-        graph.addFluxBias("L", "Z", "M")
 
-    def test_charge_biasing_rules(self):
-        graph = get_simple_test_graph()
+def test_charge_biasing_rules():
+    graph = get_simple_test_graph()
 
-        # Can add a charge bias to a node without loading
+    # Can add a charge bias to a node without loading
+    graph.addChargeBias(1, "XY")
+
+    # Cannot add a charge bias again to the same node
+    with pytest.raises(ValueError):
         graph.addChargeBias(1, "XY")
 
-        # Cannot add a charge bias again to the same node
-        self.assertRaises(ValueError, graph.addChargeBias, 1, "XY")
+    # Cannot use the same suffix twice
+    # Note node 0 should be protected, as it currently always represents ground, however
+    # it isn't clear how to enforce that without inconvenience to the user at the moment.
+    with pytest.raises(ValueError):
+        graph.addChargeBias(0, "XY")
 
-        # Cannot use the same suffix twice
-        # Note node 0 should be protected, as it currently always represents ground, however
-        # it isn't clear how to enforce that without inconvenience to the user at the moment.
-        self.assertRaises(ValueError, graph.addChargeBias, 0, "XY")
 
-    def test_flux_biasing_on_loops(self):
-        graph = CircuitGraph()
-        graph.addBranch(0, 1, "I1")
-        graph.addBranch(1, 2, "L1")
-        graph.addBranch(1, 2, "L2")
+def test_flux_biasing_on_loops():
+    graph = CircuitGraph()
+    graph.addBranch(0, 1, "I1")
+    graph.addBranch(1, 2, "L1")
+    graph.addBranch(1, 2, "L2")
 
-        # Cannot add a flux bias to an element that is not part of a superconducting loop
-        self.assertRaises(TypeError, graph.addFluxBias, "I1", "Z1")
+    # Cannot add a flux bias to an element that is not part of a superconducting loop
+    with pytest.raises(TypeError):
+        graph.addFluxBias("I1", "Z1")
 
-    def test_mutual_inductance(self):
-        graph = CircuitGraph()
-        graph.addBranch(0, 1, "L1")
-        graph.addBranch(0, 1, "I1")
-        graph.addBranch(0, 1, "C1")
-        graph.addBranch(0, 2, "L2")
-        graph.addBranch(0, 2, "I2")
-        graph.addBranch(0, 2, "C2")
 
-        # Can couple inductive branches
+def test_mutual_inductance():
+    graph = CircuitGraph()
+    graph.addBranch(0, 1, "L1")
+    graph.addBranch(0, 1, "I1")
+    graph.addBranch(0, 1, "C1")
+    graph.addBranch(0, 2, "L2")
+    graph.addBranch(0, 2, "I2")
+    graph.addBranch(0, 2, "C2")
+
+    # Can couple inductive branches
+    graph.coupleBranchesInductively("L1", "L2", "M")
+
+    # Cannot couple non inductive branches
+    with pytest.raises(TypeError):
+        graph.coupleBranchesInductively("I1", "L2", "M1")
+    with pytest.raises(TypeError):
+        graph.coupleBranchesInductively("L1", "I2", "M2")
+    with pytest.raises(TypeError):
+        graph.coupleBranchesInductively("I1", "I2", "M3")
+
+    # Cannot use the same mutual name twice
+    with pytest.raises(ValueError):
         graph.coupleBranchesInductively("L1", "L2", "M")
 
-        # Cannot couple non inductive branches
-        self.assertRaises(TypeError, graph.coupleBranchesInductively, "I1", "L2", "M1")
-        self.assertRaises(TypeError, graph.coupleBranchesInductively, "L1", "I2", "M2")
-        self.assertRaises(TypeError, graph.coupleBranchesInductively, "I1", "I2", "M3")
-
-        # Cannot use the same mutual name twice
-        self.assertRaises(ValueError, graph.coupleBranchesInductively, "L1", "L2", "M")
-
-        # Cannot couple the same inductors twice
-        self.assertRaises(ValueError, graph.coupleBranchesInductively, "L1", "L2", "M4")
+    # Cannot couple the same inductors twice
+    with pytest.raises(ValueError):
+        graph.coupleBranchesInductively("L1", "L2", "M4")
